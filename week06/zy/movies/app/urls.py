5e6fb84d059bc41208fd783293c20633fb4302c6
zhujunzhujunzhu <18715134271@163.com>
2020-08-03 07:35:55 +0800

    实现分页功能

--- week06/zy/movies/app/urls.py
+++ week06/zy/movies/app/urls.py
@@ -2,11 +2,13 @@
 @Description 
 @Autor 朱俊
 @Date 2020-08-02 00:17:53
-@LastEditors 朱俊
-@LastEditTime 2020-08-02 22:50:22
+LastEditors 朱俊
+LastEditTime 2020-08-03 07:09:35
 '''
 from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    # 已借图书查询并展示
+    path("<pindex>/", views.pager, name="pager"),
 ]
--- week06/zy/movies/app/views.py
+++ week06/zy/movies/app/views.py
@@ -7,22 +7,39 @@
 #     render(request, '你好世界')
 from django.http import HttpResponse
 from django.shortcuts import render
+import math
 # from .models import
 from .models import Movies
+from django.core.paginator import Paginator
+table_header = [
+    {'label': '名称', 'prop': 'name'},
+    {'label': '类型', 'prop': 'type'},
+    # {'label': '上映时间', 'prop': 'time'},
+    {'label': '短评', 'prop': 'short'},
+    {'label': '评分', 'prop': 'grade'}
+]
 
 
 def index(request):
     # print(request)
     # return HttpResponse("Hello, world.")
-    table_header = [
-        {'label': '名称', 'prop': 'name'},
-        {'label': '类型', 'prop': 'type'},
-        # {'label': '上映时间', 'prop': 'time'},
-        {'label': '短评', 'prop': 'short'},
-        {'label': '评分', 'prop': 'grade'}
-    ]
+
     # list = Movies.objects.all()
     # 处理星级大于3的 也就是 评分高于6的
     list = Movies.objects.filter(grade__gt=6)
 
     return render(request, 'index.html', {"table_header": table_header, "list": list})
+
+
+def pager(request, pindex):
+    movies = Movies.objects.all()  # 获取借书表中所有的数据
+    pages = range(math.ceil(Movies.objects.count() / 5))
+    pages = [i+1 for i in pages]
+    paginator = Paginator(movies, 5)  # 实例化Paginator, 每页显示5条数据
+    if pindex == "":  # django中默认返回空值，所以加以判断，并设置默认值为1
+        pindex = 1
+    else:  # 如果有返回在值，把返回值转为整数型
+        int(pindex)
+    page = paginator.page(pindex)  # 传递当前页的实例对象到前端
+    list = page.object_list
+    return render(request, 'index.html', {"table_header": table_header, "list": list, "pages": pages})
